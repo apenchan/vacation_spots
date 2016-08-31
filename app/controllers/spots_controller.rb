1c353e8484c4f791457af1e95e3fa138910feb7e
Anna Penchansky <a.penchansky@gmail.com>
2016-08-31 15:18:09 -0400

    have full CRUD and MVP

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,5 @@
 class SpotsController < ApplicationController
-  before_action :set_spot, only:[:show]
+  before_action :set_spot, only:[:show, :edit, :update, :destroy]
 
   def index
     @spots = Spot.all
@@ -8,16 +8,33 @@ class SpotsController < ApplicationController
   def show
   end
 
-  def create
+  def new
+    @spot = Spot.new
   end
 
   def edit
   end
 
+  def create
+    @spot = Spot.new(spot_params)
+     if @spot.save
+      redirect_to @spot
+    else
+      render :action => :new
+  end
+end
+
   def update
+    if @spot.update(spot_params)
+      redirect_to @spot
+    else
+      render :action => :edit
+    end
   end
 
   def destroy
+    @spot = Spot.find(params[:id]).destroy
+    redirect_to :root
   end
 
   private
